c69439fc2ff9093a80e42bca27976693b499a426
phucnguyen <phucnguyendigital2003@gmail.com>
2025-11-25 22:48:16 +0700

    find out on from s4 to s6

---
+++ s4_statisticalAnalysis/analyzing_data.py
@@ -0,0 +1,20 @@
+# Analyzing data
+# 1. Mesures of central tendency
+# - Mean
+# - Median
+# - Mode
+
+# 2. Measures of variability
+# - Standard deviation
+# - Variance
+# - Range
+
+# 3. Normal, Bionomial, and other distributions
+# - Normal distribution
+# - Bionomial distribution
+# - Poisson distribution
+
+# 4. Null and alternative hypotheses
+# - Null hypothesis
+# - Alternative hypothesis
+
---
+++ s5_introMachineLearning/relativeMachineLearning.py
@@ -0,0 +1,6 @@
+
+# 1. Feature scaling and normalization
+# 2. Encoding categorical variables
+# 3. Handling imbalanced data
+# 4. Linear and logistic regression
+
